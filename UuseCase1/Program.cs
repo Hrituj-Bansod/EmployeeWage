04c791c4f31dc0f3b3b065548bb6d5dca98c4e8f
Hrituj-Bansod <hritujbansod@gmail.com>
2024-03-27 18:32:59 +0530

    UseCase1 Added

---
+++ UuseCase1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UuseCase1
+{
+    internal class UC1
+    {
+        public void EmpPresentOrAbsent() 
+        {
+            Random random = new Random();
+            int result = random.Next(2);
+
+            if (result == 0)
+            {
+                Console.WriteLine("Employee is ABSENT");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is PRESENT");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            UC1 uc1 = new UC1();
+            uc1.EmpPresentOrAbsent();
+            Console.WriteLine("Hello");
+        }
+    }
+}
